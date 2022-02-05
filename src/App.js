71944d23aba5c5b518e3d4f497c4bc266d962629
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-05 09:38:00 -0800

    working with lists

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ const Main = (props) => {
   return (
     <section>
       <p>We serve the most {props.adjective} food around.</p>
+      <ul style={{ textAlign: 'left' }}>
+        {dishes.map((dish) => (
+          <li>{dish}</li>
+        ))}
+      </ul>
     </section>
   )
 }
@@ -25,11 +30,13 @@ const Footer = (props) => {
   )
 }
 
+const dishes = ['Mac and Cheez', 'Mushroom Yum Yum', 'Tofu with Vegetable']
+
 function App() {
   return (
     <div className='App'>
       <Header name='Frogger' />
-      <Main adjective='amazing' />
+      <Main adjective='amazing' dishes={dishes} />
       <Footer year={new Date().getFullYear()} />
     </div>
   )
