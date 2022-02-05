92f76df5e551e8e4ae3f767fc9fea0d32f586053
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-05 08:24:00 -0800

    create react components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Sev's Kitchen</h1>
+    </header>
+  )
+}
+
+const Main = () => {
+  return (
+    <section>
+      <p>We serve the most delicious food around.</p>
+    </section>
+  )
+}
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>It's true</p>
+    </footer>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-	React.createElement('h1', null, 'Hey hoes!'),
-	document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
