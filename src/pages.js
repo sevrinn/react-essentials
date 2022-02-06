9cb57a4ce941de48ad93279d1b3cd3f3ba0e76ec
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-06 07:18:29 -0800

    added nav and Links to homepage

--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export function Home() {
   return (
     <div>
       <h1>[Company Website]</h1>
+      <nav>
+        <Link to='about'>About</Link>
+        <Link to='events'>Events</Link>
+        <Link to='contact'>Contact</Link>
+      </nav>
     </div>
   )
 }
